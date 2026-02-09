df96d7318c51fbbcbe43b57cad12fcae244a9142
TuNombreApellidos <manudatos1@gmail.com>
2026-02-09 11:01:57 +0100

    CSV

--- RA5/csv/ej9.py
+++ RA5/csv/ej9.py
@@ -2,8 +2,14 @@
 clientes.csv específico por su nombre, mostrando todos los detalles de dicho clientes.csv.'''
 import csv
 intro=input("¿Que clientes.csv quieres buscar?: ")
+guardar=[]
 with open("clientes.csv", "r") as f:
     reader = csv.reader(f)
     for row in reader:
         if row[0] == intro:
             print(row)
+            guardar.append(row)
+
+with open("guardar.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerows(guardar)
